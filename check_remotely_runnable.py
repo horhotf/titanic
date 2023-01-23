2cdf896911bd0f0aceafe88c8980f9d376ac66b7
igor rytsarev <igor.rytsarev@glowbyteconsulting.com>
2023-01-23 21:25:01 +0300

    8

--- check_remotely_runnable.py
+++ check_remotely_runnable.py
@@ -29,7 +29,7 @@ def check_task_status(task_id, timeout=600):
             elif task_status == 'in_progress':
                 # Try to get the first iteration metric
                 if task.get_last_iteration() > 0:
-                    task.mark_stopped()
+                    # task.mark_stopped()
                     # task.set_archived(True)
                     return True
             time.sleep(5)
